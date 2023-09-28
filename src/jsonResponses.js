a1b8ceef4129941af022f6fd243eabe2c6fa2b44
Thomas Martinez <tjm7126@rit.edu>
2023-09-28 18:00:49 -0400

    post functionality implemented

---
+++ src/jsonResponses.js
@@ -0,0 +1,81 @@
+const users = {};
+const notRealError = 'Not Real';
+
+// responds with json object
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+// responds without body (only metadata)
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+// return user object as json
+const getUsers = (request, response, method) => {
+  console.log(method);
+  if (method === 'GET') {
+    const responseJSON = { users };
+
+    respondJSON(request, response, 200, responseJSON);
+  } else {
+    respondJSONMeta(request, response, 200, responseJSON);
+  }
+};
+
+const notReal = (request, response, method) => {
+  console.log(method);
+  if (method === 'GET') {
+    const responseJSON = { notRealError };
+
+    respondJSON(request, response, 404, responseJSON);
+  } else {
+    respondJSONMeta(request, response, 404, responseJSON);
+  }
+};
+
+const addUser = (request, response, body) => {
+  console.log(body);
+  // default response
+  const responseJSON = {
+    message: 'Name and age are both required',
+  };
+
+  // if a name and age are not given
+  if (!body.name || !body.age) {
+    responseJSON.id = 'addUserMissingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 204; // updating pre-existing user
+
+  // if this user does not exist yet
+  if (!users[body.name]) {
+    responseCode = 201; // adding new user
+    users[body.name] = {}; // create new user with given name
+  }
+
+  // set fields
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  console.log(users);
+
+  // return 201
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Succesfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  // return 204
+  return respondJSONMeta(request, response, responseCode);
+};
+
+module.exports = {
+  addUser,
+  getUsers,
+  notReal,
+};
---
+++ src/server.js
@@ -0,0 +1,72 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const parseBody = (request, response, callback) => {
+  const body = [];
+
+  // handle error
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statusCode = 400;
+    response.end();
+  });
+
+  // whenever a piece of data is recieved
+  request.on('data', (chunk) => {
+    body.push(chunk);
+  });
+
+  // when last chunk has been recieved
+  request.on('end', () => {
+    console.log(body);
+
+    const bodyString = Buffer.concat(body).toString();
+    console.log(bodyString);
+
+    const bodyParams = query.parse(bodyString);
+    console.log(bodyParams);
+
+    callback(request, response, bodyParams);
+  });
+};
+
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    '/notReal': jsonHandler.notReal,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsers,
+    '/notReal': jsonHandler.notReal,
+  },
+  POST: {
+    '/addUser': (request, response) => { parseBody(request, response, jsonHandler.addUser); },
+  },
+};
+
+const onRequest = (request, response) => {
+  // first we have to parse information from the url
+  const parsedUrl = url.parse(request.url);
+  // const params = query.parse(parsedUrl.query);
+  // const acceptedTypes = request.headers.accept.split(',');
+
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    return urlStruct[request.method][parsedUrl.pathname](request, response, request.method);
+  }
+
+  // return urlStruct.notFound(request, response, acceptedTypes);
+
+  return urlStruct[request.method]['/'];
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
